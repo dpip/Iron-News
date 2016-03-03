30ccb798be542c77d7a0d46a656c3c57d7429033
Daniel Pipkin <dpip.nc@gmail.com>
2016-03-03 01:28:05 -0500

    later

---
+++ assets/news.js
@@ -0,0 +1,13 @@
+
+      // jquerytest
+// for(i = 0; i < 10; i ++) {
+//   if(i % 5 === 2) {
+//     console.log("great");
+//   }
+// }
+
+$('.arrowClick').on('click', function() {
+
+    $(this).toggleClass('ipsums');
+
+});
